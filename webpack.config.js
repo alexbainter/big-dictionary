2852e709971af18bbd2d0c66953623bfa90388e0
metalex9 <alexbainter@gmail.com>
2017-03-23 20:52:01 -0500

    Also setup webpack.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: path.resolve(__dirname, 'public', 'src', 'definition.js'),
+    output: {
+        path: path.resolve(__dirname, 'public', 'dist'),
+        filename: 'bundled.js'
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader', 'sass-loader'],
+              }),
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ]
+};
