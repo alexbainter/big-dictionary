8cfa4dfc94c9783d875867cc3fcdbb0880b4be46
metalex9 <alexbainter@gmail.com>
2017-04-24 18:56:04 -0500

    Change throttling to 15 per day per user.

--- config/throttle.config.js
+++ config/throttle.config.js
@@ -1,7 +1,6 @@
 const throttleOptions = {
-    burst: 80,
+    burst: 15,
     period: 'day',
-    key: () => 1,   // throttle entire site
     on_throttled: (req, res, next, bucket) => {
         res.render('throttled');
     }
